a4c22f553cd7673d0975004124ff22252548ac0e
Minghe Huang <h.minghe@gmail.com>
2019-04-21 15:15:57 +0800

    add state field

--- agent.go
+++ agent.go
@@ -5,16 +5,86 @@ import (
 	"syscall"
 
 	gops "github.com/keybase/go-ps"
+	gopsutil "github.com/shirou/gopsutil/process"
 )
 
-// List list processes
-func List() ([]gops.Process, error) {
-	ps, err := gops.Processes()
+// D    uninterruptible sleep (usually IO)
+// R    running or runnable (on run queue)
+// S    interruptible sleep (waiting for an event to complete)
+// T    stopped, either by a job control signal or because it is being traced.
+// W    paging (not valid since the 2.6.xx kernel)
+// X    dead (should never be seen)
+// Z    defunct ("zombie") process, terminated but not reaped by its parent.
+
+// For BSD formats and when the stat keyword is used, additional characters may be displayed:
+// <    high-priority (not nice to other users)
+// N    low-priority (nice to other users)
+// L    has pages locked into memory (for real-time and custom IO)
+// s    is a session leader
+// l    is multi-threaded (using CLONE_THREAD, like NPTL pthreads do)
+// +    is in the foreground process group.
+
+// Process a process
+type Process struct {
+	Pid        int
+	PPid       int
+	Executable string
+	Path       string
+	State      string
+}
+
+const StateUnknown = "Unknown"
+
+func getState(pid int) string {
+	p, err := gopsutil.NewProcess(int32(pid))
 	if err != nil {
-		return []gops.Process{}, err
+		return StateUnknown
+	}
+	state, err := p.Status()
+	if err != nil {
+		return StateUnknown
+	}
+	return state
+}
+
+// Inspect inspect a process
+func Inspect(pid int) (Process, error) {
+	p, err := gops.FindProcess(pid)
+	if err != nil {
+		return Process{}, err
 	}
 
-	return ps, err
+	path, _ := p.Path()
+	return Process{
+		Pid:        pid,
+		PPid:       p.PPid(),
+		Executable: p.Executable(),
+		Path:       path,
+		State:      getState(pid),
+	}, nil
+}
+
+// List list processes
+func List() ([]Process, error) {
+	ps, err := gops.Processes()
+	if err != nil {
+		return []Process{}, err
+	}
+
+	list := []Process{}
+	for _, p := range ps {
+		pid := p.Pid()
+		path, _ := p.Path()
+		list = append(list, Process{
+			Pid:        p.Pid(),
+			PPid:       p.PPid(),
+			Path:       path,
+			Executable: p.Executable(),
+			State:      getState(pid),
+		})
+	}
+
+	return list, err
 }
 
 // Start start a binary with args
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/metrue/p
 
 require (
 	github.com/keybase/go-ps v0.0.0-20161005175911-668c8856d999
+	github.com/shirou/gopsutil v2.18.12+incompatible
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/urfave/cli v1.20.0
+	golang.org/x/sys v0.0.0-20190419153524-e8e3143a4f4a // indirect
 )
